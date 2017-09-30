2d0483fd1a7f21d4baa454bd1059b3b1482d9c4b
Michał <kwestorowski.m@gmail.com>
2017-09-30 16:51:09 +0200

    Created a little bit view of login screen, gotta make that more usable for user and i will take video as background

---
+++ src/components/LoginScreen.js
@@ -0,0 +1,54 @@
+/**
+ * Created by MKwestorowski on 30/09/2017.
+ */
+import React from 'react'
+
+
+import {connect} from 'react-redux'
+
+import './App.css'
+
+export default connect(
+    state => ({
+        contributors: state.contributors
+    }),
+    dispatch => ({
+        success: data => dispatch({
+            type: 'contributors/FETCH__SUCCESS',
+            data: data
+        })
+
+    })
+)(class Singin extends React.Component {
+
+    componentDidMount() {
+        fetch(
+            'https://api.github.com/repos/angular/angular'
+        ).then(
+            response => response.json()
+        ).then(
+            data => {
+                this.props.success(data);
+                fetch(data.contributors_url).then(
+                    response => response.json().then(
+                        data => this.props.success(data)))
+            }
+        )
+    }
+
+    render() {
+
+
+        return (
+
+            <div className="gitHubButtonWidth flex-container">
+                <a href={`http://github.com/login/oauth/authorize?client_id=9f13dc502b256fcebd4f`}
+                   className="flex-item-button btn btn-block btn-sm btn-social btn-github">
+                    <span className="fa fa-github"></span>
+                    Sign in with Github
+                </a>
+            </div>
+
+        )
+    }
+})
