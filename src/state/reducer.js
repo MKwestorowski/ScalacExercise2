c5fcebb0bb1f8909ab998b907f8f619f6203d1f9
Michał <kwestorowski.m@gmail.com>
2017-09-30 16:49:02 +0200

    Added reducer form last part

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -1,3 +1,16 @@
 /**
  * Created by MKwestorowski on 28/09/2017.
  */
+import contributors from './parts/contributors'
+import contributorLogin from './parts/contributor'
+import selectingData from './parts/selectingData'
+
+
+export default (state = {}, action) => {
+    return {
+
+        contributors: contributors(state.contributors, action),
+        contributorLogin: contributorLogin(state.contributorLogin, action),
+        selectingData: selectingData(state.selectingData, action)
+    }
+}
