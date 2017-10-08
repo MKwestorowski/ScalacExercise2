2c23334c05af367237f8cd5aa86011c0f888facb
Michał <kwestorowski.m@gmail.com>
2017-10-08 10:12:42 +0200

    Fixed with Bartosz tip

--- src/components/AngularIntroduce.js
+++ src/components/AngularIntroduce.js
@@ -10,7 +10,7 @@ export const AngularIntroduce = (props) =>{
                <img src={props.angularAvatar} alt=""/>
 <h1>Count of Repositories {props.angularReposCount}</h1>
                <h1><a href={props.angularBlog}>Look at their BLOG, there is a lot of interting materials!</a></h1>
-               <h2 onClick={props.showContributors}>Take a look at their contributors!</h2>
+               <h2 onClick={() => props.showContributors()}>Take a look at their contributors!</h2>
            </header>
         </div>
 
