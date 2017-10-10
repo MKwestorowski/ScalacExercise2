8051555540b36af7018e520f9ea0beed0c438f2c
Michał <kwestorowski.m@gmail.com>
2017-10-10 20:55:38 +0200

    Make up contributor dumb component and preparing smart component to display and set method of filter etc

---
+++ src/components/ContributorsList.js
@@ -0,0 +1,14 @@
+/**
+ * Created by MKwestorowski on 10/10/2017.
+ */
+import React from 'react'
+
+export const Contributors = () => {
+    return (
+
+        <div>
+
+        </div>
+
+    )
+}
--- src/containers/Contributors.js
+++ src/containers/Contributors.js
@@ -3,14 +3,13 @@
  */
 
 import React from 'react'
-import {AngularIntroduce} from './../components/AngularIntroduce'
+import {ContributorsList} from './../components/ContributorsList'
 
 import {connect} from 'react-redux'
 
 
 export default connect(
     state => ({
-        angularData: state.angularData,
         angularReposList: state.angularReposList
     }),
     dispatch =>  ({
@@ -25,39 +24,10 @@ export default connect(
 
 
 
+    comonentDidMount() {
+        console.log(this.props.angularReposList)
 
-
-    componentDidMount() {
-
-        this.getAngularData()
     }
-
-    getAngularData = async () => {
-        try {
-            const response = await fetch('https://api.github.com/orgs/angular');
-            const got = await response.json();
-            await this.props.success(got)
-            console.log(this.props.success)
-        }
-        catch (err) {
-            throw console.log('fetch failed', err);
-        }
-    }
-
-    getAngularContributors = async () => {
-        try {
-            const prepare = await this.props.reposList.map(e => fetch(e.contriburors))
-            const response = await fetch(this.props.angularData.data.repos_url);
-            const got = await response.json();
-            await this.props.success(got)
-            console.log(this.props.success)
-            console.log(got)
-        }
-        catch (err) {
-            throw console.log('fetch failed', err);
-        }
-    }
-
 
 
 
@@ -65,17 +35,13 @@ export default connect(
     render() {
 
 
-        const angularContributors = this.getAngularContributors
-        const angularData = this.props.angularData.data
-        console.log(this.props.angularData)
-        console.log(angularContributors)
 
         return (
-            <AngularIntroduce
-                showContributors={angularContributors}
-                angularBlog={angularData.blog}
-                angularReposCount={angularData.public_repos}
-                angularAvatar={angularData.avatar_url}/>
+            <ContributorList
+                showContributors={}
+                angularBlog={}
+                angularReposCount={}
+                angularAvatar={}/>
 
         )
     }
